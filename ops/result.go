04ee29b6a55ade2de10467e8dddef143ff30bd1e
xuender <xuender@gmail.com>
2025-01-08 08:56:21 +0800

    doc: ops

--- ops/result.go
+++ ops/result.go
@@ -1,35 +1,26 @@
 package ops
 
-// Result is a generic function that returns the provided argument unchanged.
-// The variadic parameter is ignored and can be used for potential extensions.
+// Result returns the provided argument unchanged.
 func Result[R any](arg R, _ ...any) R {
 	return arg
 }
 
-// Result1 is a generic function that acts as an identity function,
-// returning the provided argument unchanged. The additional variadic
-// parameters are ignored and can be used for future extensions.
+// Result1 returns the provided argument unchanged.
 func Result1[R any](arg R, _ ...any) R {
 	return arg
 }
 
-// Result2 is a generic function that returns the second argument unchanged.
-// It ignores the first argument and any additional variadic parameters.
-// This function can be used as an identity function for the second argument provided.
+// Result2 returns the second provided argument unchanged.
 func Result2[R any](_ any, arg R, _ ...any) R {
 	return arg
 }
 
-// Result3 is a generic function that returns the third argument unchanged.
-// It ignores the first two arguments and any additional variadic parameters.
-// This function serves as an identity function for the third argument provided.
+// Result3 returns the third provided argument unchanged.
 func Result3[R any](_, _ any, arg R, _ ...any) R {
 	return arg
 }
 
-// Result4 is a generic function that returns the fourth argument unchanged.
-// It ignores the first three arguments and any additional variadic parameters.
-// This function serves as an identity function for the fourth argument provided.
+// Result4 returns the fourth provided argument unchanged.
 func Result4[R any](_, _, _ any, arg R, _ ...any) R {
 	return arg
 }

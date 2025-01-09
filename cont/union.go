3c1a6585573d73307c4afbeca39e57da9553885f
xuender <xuender@gmail.com>
2025-01-09 08:50:33 +0800

    feat: cont add UnionFunc

--- cont/union.go
+++ cont/union.go
@@ -1,5 +1,7 @@
 package cont
 
+import "slices"
+
 // Union returns a slice containing all unique elements from the provided slices.
 func Union[S ~[]E, E comparable](items ...S) S {
 	if len(items) == 0 {
@@ -16,6 +18,30 @@ func Union[S ~[]E, E comparable](items ...S) S {
 			}
 
 			set.Add(item)
+			union = append(union, item)
+		}
+	}
+
+	return union
+}
+
+// UnionFunc computes the union of multiple slices using a custom equality function.
+// It returns a new slice containing unique elements from all input slices based on the provided comparator.
+func UnionFunc[S ~[]E, E any](equal func(E, E) bool, items ...S) S {
+	if len(items) == 0 {
+		return nil
+	}
+
+	union := []E{}
+
+	for _, slice := range items {
+		for _, item := range slice {
+			if slices.ContainsFunc(union, func(val E) bool {
+				return equal(val, item)
+			}) {
+				continue
+			}
+
 			union = append(union, item)
 		}
 	}
--- cont/union_example_test.go
+++ cont/union_example_test.go
@@ -8,8 +8,21 @@ import (
 
 func ExampleUnion() {
 	fmt.Println(cont.Union[[]int]())
-	fmt.Println(cont.Union([]int{1}))
+	fmt.Println(cont.Union([]int{1, 1}))
 	fmt.Println(cont.Union([]int{1, 2}, []int{2, 3}))
+
+	// Output:
+	// []
+	// [1]
+	// [1 2 3]
+}
+
+func ExampleUnionFunc() {
+	equal := func(a, b int) bool { return a == b }
+
+	fmt.Println(cont.UnionFunc[[]int](equal))
+	fmt.Println(cont.UnionFunc(equal, []int{1, 1}))
+	fmt.Println(cont.UnionFunc(equal, []int{1, 2}, []int{2, 3}))
 
 	// Output:
 	// []

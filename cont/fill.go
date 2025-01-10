b11a31640ecd1fa4ae023516614395e9b9d9918b
xuender <xuender@gmail.com>
2025-01-10 09:49:34 +0800

    feat: add cont.Find

--- cont/fill.go
+++ cont/fill.go
@@ -1,5 +1,8 @@
 package cont
 
+// Fill assigns the value `val` to elements of the slice from index `start` to `end`.
+// If `start` and `end` are not provided, it fills the entire slice.
+// If only `start` is provided, it fills from `start` to the end of the slice.
 func Fill[S ~[]E, E any](slice S, val E, startAndEnd ...int) {
 	end := len(slice)
 	if end == 0 {
